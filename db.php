38e439a490dfdc954a36c83e10fa671eb053109f
Hania AiWali <haniaaiwali006@gmail.com>
2026-03-05 12:50:50 +0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$db = 'rsoa_rsoa278_32';
+$user = 'rsoa_rsoa278_32';
+$pass = '123456';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
